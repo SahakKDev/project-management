508b6470e3baabd71e48cf3a84752feeff68e024
Sahak <sahak.keshishyan.dev@gmail.com>
2024-01-17 17:10:37 +0400

    create sidebar and main content, add new project functionality done, validation in progress

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import SideBar from "./components/Sidebar";
+
+function App() {
+  /*
+    undefined: There is no selectedProject
+    null: We want to add a new project
+    [ID]: selected project id
+  */
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+  const { selectedProject, projects } = projectsState;
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProject: null,
+    }));
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProjectId = Math.random() * Math.random();
+
+      const newProject = {
+        ...projectData,
+        id: newProjectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProject: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log("projects", projects);
+
+  let content;
+  if (selectedProject === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar onStartAddProject={handleStartAddProject} projects={projects} />
+      {content}
+    </main>
+  );
+}
+
+export default App;
